ee11d231ccee3b2f433306e1aa341a18c8c52604
Diego <anikilogomez123@gmail.com>
2025-08-29 15:18:15 -0300

    feat(test-auth): initial test files and setup

---
+++ test/org.pos.software.test/IntegrationTest/AuthControllerTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace org.pos.software.test.IntegrationTest
+{
+    public class AuthControllerTest
+    {
+
+        [Fact]
+        public async Task Login_ValidCredentials_ReturnsOk()
+        {
+            // Arrange
+            // Action
+            // Assert
+        }
+
+        [Fact]
+        public async Task Login_InvalidPassword_ReturnsUnauthorized()
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
+
+    }
+}
---
+++ test/org.pos.software.test/UnitTest/AuthServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace org.pos.software.test.UnitTest
+{
+    public class AuthServiceTest
+    {
+
+        [Fact]
+        public void Login_ValidCredentials_ReturnsToken()
+        {
+            // Arrange
+            // Action
+            // Assert
+        }
+
+        [Fact]
+        public void Login_InvalidPassword_ThrowsUnauthorizedException()
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
+
+    }
+}
