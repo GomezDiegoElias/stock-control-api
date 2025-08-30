57fb43c2930e756bc3e0d3a26ca48d950130d0fd
Diego <anikilogomez123@gmail.com>
2025-08-30 17:34:01 -0300

    feat(infrastructure): add repository implementation

--- src/org.pos.software/Infrastructure/Persistence/SqlServer/Repositories/UserRepository.cs
+++ src/org.pos.software/Infrastructure/Persistence/SqlServer/Repositories/UserRepository.cs
@@ -1,35 +1,79 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using org.pos.software.Domain.Entities;
-using org.pos.software.Infrastructure.Persistence.SqlServer;
-using org.pos.software.Infrastructure.Persistence.SqlServer.Entities;
+using org.pos.software.Domain.OutPort;
 using org.pos.software.Infrastructure.Persistence.SqlServer.Mappers;
-using org.pos.software.Infrastructure.Rest.Dto.Response;
 
 namespace org.pos.software.Infrastructure.Persistence.SqlServer.Repositories
 {
-    public class UserRepository
+    public class UserRepository : IUserRepository
     {
 
-        private readonly AppDbContext _dbContext;
+        private readonly AppDbContext _context;
 
-        public UserRepository(AppDbContext dbContext)
+        public UserRepository(AppDbContext context)
         {
-            _dbContext = dbContext;
+            _context = context;
         }
 
         public async Task<List<User>> FindAll()
         {
-            //List<UserEntity> entities = await _dbContext.Users.ToListAsync();
-            //List<User> users = entities.Select(UserMapper.ToDomain).ToList();
-            //return users;
-            return null;
+
+            //var entities = await _context.Users.ToListAsync();
+
+            var entities = await _context.Users
+                .Include(u => u.Role)                // trae el rol
+                .ThenInclude(r => r.RolePermissions) // trae los permisos
+                .ThenInclude(rp => rp.Permission)    // trae los permisos individuales
+                .ToListAsync();
+
+            List<User> users = entities.Select(UserMapper.ToDomain).ToList();
+            return users;
+
         }
 
-        public async Task<User> FindById(int id)
+        public async Task<User?> FindByDni(long dni)
         {
-            //var entity = await _dbContext.Users.FindAsync(id) ?? throw new KeyNotFoundException($"User with ID {id} not found.");
-            //return UserMapper.ToDomain(entity);
-            return null;
+
+            var entity = await _context.Users
+                .Include(u => u.Role)
+                .ThenInclude(r => r.RolePermissions)
+                .ThenInclude(rp => rp.Permission)
+                .FirstOrDefaultAsync(u => u.Dni == dni);
+
+            return entity == null ? null : UserMapper.ToDomain(entity);
+
+        }
+
+        public async Task<User?> FindByEmail(string email)
+        {
+            var entity = await _context.Users
+                .Include(u => u.Role)
+                    .ThenInclude(r => r.RolePermissions)
+                        .ThenInclude(rp => rp.Permission)
+                .FirstOrDefaultAsync(u => u.Email == email);
+
+            return entity == null ? null : UserMapper.ToDomain(entity);
+        }
+
+
+        public async Task<User> Save(User user)
+        {
+
+            // Obtiene RoleEntity según el Role de Domain
+            var roleEntity = await _context.Roles
+                .Include(r => r.RolePermissions)
+                    .ThenInclude(rp => rp.Permission)
+                .FirstOrDefaultAsync(r => r.Name == user.Role.Name);
+
+            if (roleEntity == null)
+                throw new ApplicationException($"Role {user.Role.Name} does not exist");
+
+            var entity = UserMapper.toEntity(user, roleEntity);
+            _context.Users.Add(entity);
+            await _context.SaveChangesAsync();
+
+            return UserMapper.ToDomain(entity);
+
         }
 
     }
