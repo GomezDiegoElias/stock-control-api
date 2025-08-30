d2fdbde4f6afa31fa9fe79232d6b66f50edd25d8
Diego <anikilogomez123@gmail.com>
2025-08-30 15:19:43 -0300

    feat(infrastructure): updates the database context by adding the new entities

--- src/org.pos.software/Infrastructure/Persistence/SqlServer/AppDbContext.cs
+++ src/org.pos.software/Infrastructure/Persistence/SqlServer/AppDbContext.cs
@@ -9,6 +9,55 @@ namespace org.pos.software.Infrastructure.Persistence.SqlServer
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<UserEntity> Users { get; set; }
+        public DbSet<RoleEntity> Roles { get; set; }
+        public DbSet<PermissionEntity> Permissions { get; set; }
+        public DbSet<RolePermissionEntity> RolePermissions { get; set; }
+
+        // Configuracion del modelo de datos
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<UserEntity>()
+                .HasIndex(u => u.Email) // Indice unico en el campo Email
+                .IsUnique();
+
+            modelBuilder.Entity<UserEntity>()
+                .HasIndex(u => u.Dni) // Indice unico en el campo Dni
+                .IsUnique();
+
+            modelBuilder.Entity<UserEntity>()
+                .Property(u => u.Status)
+                .HasConversion<string>(); // Almacenar Status como string
+
+            modelBuilder.Entity<RolePermissionEntity>()
+                .HasOne(rp => rp.Role)
+                .WithMany(r => r.RolePermissions)
+                .HasForeignKey(rp => rp.RoleId);
+
+            modelBuilder.Entity<RolePermissionEntity>()
+                .HasOne(rp => rp.Permission)
+                .WithMany(p => p.RolePermissions)
+                .HasForeignKey(rp => rp.PermissionId);
+
+            modelBuilder.Entity<UserEntity>()
+                .HasOne(u => u.Role)
+                .WithMany(r => r.Users)
+                .HasForeignKey(u => u.RoleId)
+                .IsRequired();
+
+            modelBuilder.Entity<RolePermissionEntity>()
+                .HasOne(rp => rp.Role)
+                .WithMany(r => r.RolePermissions)
+                .HasForeignKey(rp => rp.RoleId)
+                .IsRequired();
+
+            modelBuilder.Entity<RolePermissionEntity>()
+                .HasOne(rp => rp.Permission)
+                .WithMany(p => p.RolePermissions)
+                .HasForeignKey(rp => rp.PermissionId)
+                .IsRequired();
+
+        }
 
     }
 }
