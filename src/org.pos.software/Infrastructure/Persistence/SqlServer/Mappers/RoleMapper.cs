0c678cd4391f0b8c65525ad8a3469daf418fd77a
Diego <anikilogomez123@gmail.com>
2025-08-30 15:16:35 -0300

    feat(infrastructure): add role mapper

---
+++ src/org.pos.software/Infrastructure/Persistence/SqlServer/Mappers/RoleMapper.cs
@@ -0,0 +1,29 @@
+﻿using org.pos.software.Domain.Entities;
+using org.pos.software.Infrastructure.Persistence.SqlServer.Entities;
+
+namespace org.pos.software.Infrastructure.Persistence.SqlServer.Mappers
+{
+    public static class RoleMapper
+    {
+
+        public static Role ToDomain(RoleEntity entity)
+        {
+            return new Role(
+                entity.Name,
+                entity.RolePermissions.Select(rp => rp.Permission.Name)
+            );
+        }
+
+        public static RoleEntity toEntity(Role domain)
+        {
+            return new RoleEntity
+            {
+                Name = domain.Name,
+                RolePermissions = domain.Permissions
+                    .Select(p => new RolePermissionEntity { Permission = new PermissionEntity { Name = p } })
+                    .ToList()
+            };
+        }
+
+    }
+}
