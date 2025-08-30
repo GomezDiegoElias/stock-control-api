9f19c96e84671329a66b77d283441926506ea4c6
Diego <anikilogomez123@gmail.com>
2025-08-30 02:17:52 -0300

    refactor: change the return type of the domain in the save method

--- src/org.pos.software/Domain/OutPort/IRoleRepository.cs
+++ src/org.pos.software/Domain/OutPort/IRoleRepository.cs
@@ -6,7 +6,7 @@ namespace org.pos.software.Domain.OutPort
     public interface IRoleRepository
     {
         public Task<Role> FindByName(string name);
-        public Task<RoleEntity> Save(RoleEntity role);
+        public Task<Role> Save(Role role);
         public Task<List<RoleEntity>> FindAll();
     }
 }
