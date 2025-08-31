9b4cbfabfe4cc304fffbb918cff7ee758721f6e5
Diego <anikilogomez123@gmail.com>
2025-08-31 18:49:58 -0300

    feat(domain): add exception for user

---
+++ src/org.pos.software/Domain/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,8 @@
+﻿namespace org.pos.software.Domain.Exceptions
+{
+    public class UserNotFoundException : Exception
+    {
+        public UserNotFoundException(string dni)
+            : base($"User whit DNI {dni} not found") { }
+    }
+}
