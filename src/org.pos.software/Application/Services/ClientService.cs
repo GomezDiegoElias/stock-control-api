2cb1feae108b2767ce575a6c944d349cb8b7757c
Diego <anikilogomez123@gmail.com>
2025-09-01 17:06:22 -0300

    feat(client): agrega la implementacion de los casos de uso

---
+++ src/org.pos.software/Application/Services/ClientService.cs
@@ -0,0 +1,44 @@
+﻿using org.pos.software.Application.InPort;
+using org.pos.software.Domain.Entities;
+using org.pos.software.Domain.OutPort;
+
+namespace org.pos.software.Application.Services
+{
+
+    public class ClientService : IClientService
+    {
+
+        private readonly IClientRepository _repository;
+
+        public ClientService(IClientRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<Client> Delete(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Client> FindAll(int pageIndex, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Client> FindByDni(long dni)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Client> Save(Client client)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Client> Update(Client client)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
