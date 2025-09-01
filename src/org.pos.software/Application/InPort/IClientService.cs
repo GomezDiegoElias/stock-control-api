60a79fa3e595dfcf07383476b0ec453c80f6c332
Diego <anikilogomez123@gmail.com>
2025-09-01 17:05:29 -0300

    feat(client): agrega la interfaz para casos de uso de clientes

---
+++ src/org.pos.software/Application/InPort/IClientService.cs
@@ -0,0 +1,13 @@
+﻿using org.pos.software.Domain.Entities;
+
+namespace org.pos.software.Application.InPort
+{
+    public interface IClientService
+    {
+        public Task<Client> FindAll(int pageIndex, int pageSize);
+        public Task<Client> FindByDni(long dni);
+        public Task<Client> Save(Client client);
+        public Task<Client> Update(Client client);
+        public Task<Client> Delete(long id);
+    }
+}
