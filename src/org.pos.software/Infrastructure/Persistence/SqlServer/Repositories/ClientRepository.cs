f89b2a93ddf0eca2a58972f0926db3c6cb035d41
Diego <anikilogomez123@gmail.com>
2025-09-01 17:03:21 -0300

    feat(client): agrega la implementacion del puerto de salida

---
+++ src/org.pos.software/Infrastructure/Persistence/SqlServer/Repositories/ClientRepository.cs
@@ -0,0 +1,42 @@
+﻿using org.pos.software.Domain.Entities;
+using org.pos.software.Domain.OutPort;
+
+namespace org.pos.software.Infrastructure.Persistence.SqlServer.Repositories
+{
+    public class ClientRepository : IClientRepository
+    {
+
+        private readonly AppDbContext _context;
+
+        public ClientRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<Client> Delete(long dni)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Client> FindAll(int pageIndex, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Client> FindByDni(long dni)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Client> Save(Client client)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Client> Update(Client client)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
