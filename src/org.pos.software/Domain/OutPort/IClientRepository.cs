4840bf42f536366b2a398d6ebcda8acc7785966a
Diego <anikilogomez123@gmail.com>
2025-09-01 17:02:33 -0300

    feat(client): agrega interfaz de puerto de salida

---
+++ src/org.pos.software/Domain/OutPort/IClientRepository.cs
@@ -0,0 +1,13 @@
+﻿using org.pos.software.Domain.Entities;
+
+namespace org.pos.software.Domain.OutPort
+{
+    public interface IClientRepository
+    {
+        public Task<Client> FindAll(int pageIndex, int pageSize);
+        public Task<Client> FindByDni(long dni);
+        public Task<Client> Save(Client client);
+        public Task<Client> Update(Client client);
+        public Task<Client> Delete(long dni);
+    }
+}
