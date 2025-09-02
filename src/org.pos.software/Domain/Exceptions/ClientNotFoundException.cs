8884bee9a3ff3b2a2f0ec33508ff4a4a4cd7d403
Diego <anikilogomez123@gmail.com>
2025-09-02 00:59:48 -0300

    feat(client): agrega excepcion personalizada

---
+++ src/org.pos.software/Domain/Exceptions/ClientNotFoundException.cs
@@ -0,0 +1,8 @@
+﻿namespace org.pos.software.Domain.Exceptions
+{
+    public class ClientNotFoundException : Exception
+    {
+        public ClientNotFoundException(string dni)
+            : base($"Cliente con DNI {dni} no existe") {  }
+    }
+}
