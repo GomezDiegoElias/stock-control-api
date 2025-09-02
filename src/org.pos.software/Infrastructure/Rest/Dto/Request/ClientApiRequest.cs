f36bbd82492f0cfdc81abcc87094b58d2b223092
Diego <anikilogomez123@gmail.com>
2025-09-02 00:25:35 -0300

    feat(client): agrega dto para peticiones de creacion de cliente

---
+++ src/org.pos.software/Infrastructure/Rest/Dto/Request/ClientApiRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace org.pos.software.Infrastructure.Rest.Dto.Request
+{
+    public record ClientApiRequest(
+        long Dni,
+        string FirstName,
+        string Address
+    ) { }
+}
