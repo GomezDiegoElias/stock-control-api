381ee14e49715a14d66beb98caead7b94bb74814
Diego <anikilogomez123@gmail.com>
2025-09-02 01:02:02 -0300

    feat(client): agrega manejo de nulos en el repositorio

--- src/org.pos.software/Domain/OutPort/IClientRepository.cs
+++ src/org.pos.software/Domain/OutPort/IClientRepository.cs
@@ -5,7 +5,7 @@ namespace org.pos.software.Domain.OutPort
     public interface IClientRepository
     {
         public Task<Client> FindAll(int pageIndex, int pageSize);
-        public Task<Client> FindByDni(long dni);
+        public Task<Client?> FindByDni(long dni);
         public Task<Client> Save(Client client);
         public Task<Client> Update(Client client);
         public Task<Client> Delete(long dni);
