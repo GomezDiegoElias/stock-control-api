b10fa01faa89c7f99f09fe40f5b7b762fee8395f
Diego <anikilogomez123@gmail.com>
2025-09-03 14:11:01 -0300

    feat: agrega mas campos en el dto de respuesta de paginacion

--- src/org.pos.software/Infrastructure/Rest/Dto/Response/General/PaginatedResponse.cs
+++ src/org.pos.software/Infrastructure/Rest/Dto/Response/General/PaginatedResponse.cs
@@ -4,5 +4,13 @@
     {
         public List<T> Items { get; set; }
         public int TotalCount { get; set; }
+        public int PageIndex { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages { get; set; }
+        public bool HasPreviousPage => PageIndex > 1;
+        public bool HasNextPage => PageIndex < TotalPages;
+
     }
+
+        
 }
