e6d684c70c8ea76e3aafe5cdcfab44cb1d123f0e
Diego <anikilogomez123@gmail.com>
2025-09-03 14:22:45 -0300

    refactor: cambia nombre del campo totalcount

--- src/org.pos.software/Infrastructure/Rest/Dto/Response/General/PaginatedResponse.cs
+++ src/org.pos.software/Infrastructure/Rest/Dto/Response/General/PaginatedResponse.cs
@@ -5,7 +5,7 @@
         public List<T> Items { get; set; }
         public int PageIndex { get; set; }
         public int PageSize { get; set; }
-        public int TotalCount { get; set; }
+        public int TotalItems { get; set; }
         public int TotalPages { get; set; }
         public bool HasPreviousPage => PageIndex > 1;
         public bool HasNextPage => PageIndex < TotalPages;
