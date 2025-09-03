25419339b1a00afa4a8516c6502c97962c2d8c28
Diego <anikilogomez123@gmail.com>
2025-09-03 14:15:44 -0300

    feat(client)!: cambia el tipo de retorno a paginacion en el servicio

--- src/org.pos.software/Application/InPort/IClientService.cs
+++ src/org.pos.software/Application/InPort/IClientService.cs
@@ -1,10 +1,11 @@
 ﻿using org.pos.software.Domain.Entities;
+using org.pos.software.Infrastructure.Rest.Dto.Response.General;
 
 namespace org.pos.software.Application.InPort
 {
     public interface IClientService
     {
-        public Task<Client> FindAll(int pageIndex, int pageSize);
+        public Task<PaginatedResponse<Client>> FindAll(int pageIndex, int pageSize);
         public Task<Client?> FindByDni(long dni);
         public Task<Client> Save(Client client);
         public Task<Client> Update(Client client);
