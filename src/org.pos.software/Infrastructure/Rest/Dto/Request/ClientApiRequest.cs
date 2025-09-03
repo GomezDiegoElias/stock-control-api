97d84a61015b0d814a2c137108687ac30058943e
Diego <anikilogomez123@gmail.com>
2025-09-03 17:32:21 -0300

    refactor(client): cambio de record a clases con get y set para patch

--- src/org.pos.software/Infrastructure/Rest/Dto/Request/ClientApiRequest.cs
+++ src/org.pos.software/Infrastructure/Rest/Dto/Request/ClientApiRequest.cs
@@ -1,8 +1,11 @@
 ﻿namespace org.pos.software.Infrastructure.Rest.Dto.Request
 {
-    public record ClientApiRequest(
-        long Dni,
-        string FirstName,
-        string Address
-    ) { }
+    public class ClientApiRequest
+    {
+        public long Dni { get; set; }
+        public string FirstName { get; set; }
+        public string Address { get; set; }
+
+        public ClientApiRequest() { }
+    }
 }
