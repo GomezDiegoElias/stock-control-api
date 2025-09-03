7ee62cb7b43d862f20c698bb5ae3c5f3257710d5
Diego <anikilogomez123@gmail.com>
2025-09-03 14:19:02 -0300

    refactor: cambia el nombre del campo total de registros

--- src/org.pos.software/Infrastructure/Rest/Dto/Response/General/PaginatedResponse.cs
+++ src/org.pos.software/Infrastructure/Rest/Dto/Response/General/PaginatedResponse.cs
@@ -3,9 +3,9 @@
     public class PaginatedResponse<T>
     {
         public List<T> Items { get; set; }
-        public int TotalCount { get; set; }
         public int PageIndex { get; set; }
         public int PageSize { get; set; }
+        public int TotalCount { get; set; }
         public int TotalPages { get; set; }
         public bool HasPreviousPage => PageIndex > 1;
         public bool HasNextPage => PageIndex < TotalPages;
