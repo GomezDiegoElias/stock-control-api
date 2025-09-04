3e3806932114b5873e4286817b02a688cab1499b
Diego <anikilogomez123@gmail.com>
2025-09-04 01:00:51 -0300

    feat(role): agrega endpoint para actualizar permisos a roles especificos

---
+++ src/org.pos.software/Infrastructure/Rest/Controllers/RoleController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using org.pos.software.Application.InPort;
+using org.pos.software.Infrastructure.Rest.Dto.Request;
+
+namespace org.pos.software.Infrastructure.Rest.Controllers
+{
+
+    [Authorize(Roles = "ADMIN")]
+    [ApiController]
+    [Route("api/v1/roles")]
+    public class RoleController : Controller
+    {
+
+        private readonly IRoleService _roleService;
+
+        public RoleController(IRoleService roleService)
+        {
+            _roleService = roleService;
+        }
+
+        [AllowAnonymous]
+        [HttpPatch("{roleName}/permissions")]
+        public async Task<IActionResult> UpdateRolePermissions(
+            string roleName,
+            [FromBody] UpdateRolePermissionsRequest request
+        )
+        {
+
+            var updatedRole = await _roleService.UpdateRolePermissions(
+                roleName,
+                request.AddPermissions ?? new string[0],
+                request.RemovePermissions ?? new string[0]
+            );
+
+            return Ok(new
+            {
+                Success = true,
+                Message = $"Permisos del rol '{roleName}' actualizados correctamente.",
+                Data = updatedRole
+            });
+
+        }
+
+    }
+
+}
