74839051bc78c328302b7c7e619768054f219a05
Diego <anikilogomez123@gmail.com>
2025-09-04 15:56:31 -0300

    feat(user): agrega metodo para borrado permanente en el repositorio

--- src/org.pos.software/Domain/OutPort/IUserRepository.cs
+++ src/org.pos.software/Domain/OutPort/IUserRepository.cs
@@ -6,9 +6,9 @@ namespace org.pos.software.Domain.OutPort
     public interface IUserRepository
     {
         public Task<PaginatedResponse<User>> FindAll(int pageIndex, int pageSize);
-        //public Task<User?> FindById(int id);
         public Task<User?> FindByDni(long dni);
         public Task<User> Save(User user);
         public Task<User?> FindByEmail(string email);
+        public Task<User> DeletePermanent(long dni);
     }
 }
