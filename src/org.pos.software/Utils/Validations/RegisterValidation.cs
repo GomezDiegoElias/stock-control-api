0ec3acb0aa6a1e0bca5699cc7519933125bed445
Diego <anikilogomez123@gmail.com>
2025-09-04 00:37:42 -0300

    refactor: validacion de registro en espanol

--- src/org.pos.software/Utils/Validations/RegisterValidation.cs
+++ src/org.pos.software/Utils/Validations/RegisterValidation.cs
@@ -8,17 +8,17 @@ namespace org.pos.software.Utils.Validations
 
         public RegisterValidation() {
             RuleFor(x => x.Dni)
-                .NotEmpty().WithMessage("DNI is required.")
-                .GreaterThan(0).WithMessage("DNI must be a positive number.");
+                .NotEmpty().WithMessage("El DNI es obligatorio.")
+                .GreaterThan(0).WithMessage("El DNI debe ser un número positivo.");
             RuleFor(x => x.Email)
-                .NotEmpty().WithMessage("Email is required.")
-                .EmailAddress().WithMessage("Invalid email format.");
+                .NotEmpty().WithMessage("El correo electronico es obligatorio.")
+                .EmailAddress().WithMessage("Formato invalido de correo electronico.");
             RuleFor(x => x.Password)
-                .NotEmpty().WithMessage("Password is required.")
-                .MinimumLength(6).WithMessage("Password must be at least 6 characters long.");
+                .NotEmpty().WithMessage("La contraseña es obligatorio.")
+                .MinimumLength(6).WithMessage("La contraseña debe tener al menos 6 caracteres..");
             RuleFor(x => x.FirstName)
-                .NotEmpty().WithMessage("First name is required.")
-                .MaximumLength(50).WithMessage("First name must not exceed 50 characters.");
+                .NotEmpty().WithMessage("El nombre es obligatorio.")
+                .MaximumLength(50).WithMessage("El nombre no debe exceder los 50 caracteres.");
         }
 
     }
