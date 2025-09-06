29be1c3a8742f90f4fc407da4a93d47e04f1e345
Diego <anikilogomez123@gmail.com>
2025-09-05 21:38:43 -0300

    feat(employee): agrega sedeer para creacion de paginado de empleado

--- src/org.pos.software/Utils/Seeder/DbSedder.cs
+++ src/org.pos.software/Utils/Seeder/DbSedder.cs
@@ -235,6 +235,37 @@ namespace org.pos.software.Utils.Seeder
             ");
         }
 
+        public static async Task SeedStoredProceduresPaginationEmployee(AppDbContext context)
+        {
+            await context.Database.ExecuteSqlRawAsync(@"
+            IF OBJECT_ID('getEmployeePagination', 'P') IS NOT NULL
+                DROP PROCEDURE getEmployeePagination;
+            ");
+
+            await context.Database.ExecuteSqlRawAsync(@"
+                CREATE PROCEDURE getEmployeePagination 
+                    @PageIndex INT = 1,
+                    @PageSize INT = 10
+                AS
+                BEGIN
+                    DECLARE @Offset INT = (@PageSize * (@PageIndex - 1));
+
+                    SELECT
+                        e.dni,
+                        e.first_name,
+                        e.last_name,
+                        e.workstation,
+                        ROW_NUMBER() OVER(ORDER BY e.dni ASC) AS Fila,
+                        COUNT(*) OVER() AS TotalFilas
+                    FROM tbl_employee e
+                    WHERE e.is_deleted = 0
+                    ORDER BY e.first_name ASC
+                    OFFSET @Offset ROWS
+                    FETCH NEXT @PageSize ROWS ONLY
+                END
+            ");
+        }
+
         ///////////////////////////////////////////////////////////////
 
     }
