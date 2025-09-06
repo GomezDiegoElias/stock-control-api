3128c73936781217ca22d2f00b575fb71bfd8359
Diego <anikilogomez123@gmail.com>
2025-09-05 21:46:58 -0300

    feat(employee): implementa el metodo de paginacion de empleado del repositorio

---
+++ src/org.pos.software/Infrastructure/Persistence/SqlServer/Repositories/EmployeeRepository.cs
@@ -0,0 +1,22 @@
+﻿using org.pos.software.Domain.Entities;
+using org.pos.software.Domain.OutPort;
+using org.pos.software.Infrastructure.Rest.Dto.Response.General;
+
+namespace org.pos.software.Infrastructure.Persistence.SqlServer.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+
+        private readonly AppDbContext _context;
+
+        public EmployeeRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<PaginatedResponse<Employee>> FindAll(int pageIndex, int pageSize)
+        {
+            return await _context.getEmployeePagination(pageIndex, pageSize);
+        }
+    }
+}
