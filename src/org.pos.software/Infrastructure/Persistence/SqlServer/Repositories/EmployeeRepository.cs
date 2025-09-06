7159287a6ef28db1b5ac51b1d53e81d6e75eb754
Diego <anikilogomez123@gmail.com>
2025-09-06 16:37:14 -0300

    feat(employee): implementa el nuevo metodo de buscar por dni del repositorio

--- src/org.pos.software/Infrastructure/Persistence/SqlServer/Repositories/EmployeeRepository.cs
+++ src/org.pos.software/Infrastructure/Persistence/SqlServer/Repositories/EmployeeRepository.cs
@@ -1,5 +1,7 @@
-﻿using org.pos.software.Domain.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using org.pos.software.Domain.Entities;
 using org.pos.software.Domain.OutPort;
+using org.pos.software.Infrastructure.Persistence.SqlServer.Mappers;
 using org.pos.software.Infrastructure.Rest.Dto.Response.General;
 
 namespace org.pos.software.Infrastructure.Persistence.SqlServer.Repositories
@@ -17,6 +19,12 @@ namespace org.pos.software.Infrastructure.Persistence.SqlServer.Repositories
         public async Task<PaginatedResponse<Employee>> FindAll(int pageIndex, int pageSize, int? dni, string? firstname, string? lastname, string? workstation)
         {
             return await _context.getEmployeePagination(pageIndex, pageSize, dni, firstname, lastname, workstation);
+        }
+
+        public async Task<Employee?> FindByDni(long dni)
+        {
+            var entity = await _context.Employees.FirstOrDefaultAsync(e => e.Dni == dni);
+            return entity == null ? null : EmployeeMapper.ToDomain(entity);
         }
     }
 }
