bdf3887b716be00d8015f3de3aa4a557ae98e8dc
Diego <anikilogomez123@gmail.com>
2025-09-05 21:44:19 -0300

    feat(employee): agrega dto para creacion de empleado

---
+++ src/org.pos.software/Infrastructure/Rest/Dto/Request/EmployeeApiRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace org.pos.software.Infrastructure.Rest.Dto.Request
+{
+    public record EmployeeApiRequest(
+        long Dni,
+        string FirstName,
+        string LastName,
+        string WorkStation
+    ) { }
+}
