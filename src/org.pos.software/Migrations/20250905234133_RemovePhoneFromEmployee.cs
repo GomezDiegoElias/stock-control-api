07ad737d8d271dcb795e0aeff03a70eab494ed1e
Diego <anikilogomez123@gmail.com>
2025-09-05 21:49:39 -0300

    feat: prueba de migraciones

---
+++ src/org.pos.software/Migrations/20250905234133_RemovePhoneFromEmployee.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace org.pos.software.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovePhoneFromEmployee : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "phone",
+                table: "tbl_employee");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "phone",
+                table: "tbl_employee",
+                type: "nvarchar(15)",
+                maxLength: 15,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
---
+++ src/org.pos.software/Migrations/20250905235046_AddPaginationEmployeeStoredProcedure.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace org.pos.software.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPaginationEmployeeStoredProcedure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                CREATE PROCEDURE getEmployeePagination 
+                    @PageIndex INT = 1,
+                    @PageSize INT = 10
+                AS
+                BEGIN
+                    DECLARE @Offset INT = (@PageSize * (@PageIndex - 1));
+
+                    SELECT
+                        e.dni,
+                        e.first_name,
+		                e.last_name,
+                        e.workstation,
+                        ROW_NUMBER() OVER(ORDER BY e.dni ASC) AS Fila,
+                        COUNT(*) OVER() AS TotalFilas
+                    FROM tbl_employee e
+	                WHERE e.is_deleted = 0
+                    ORDER BY e.first_name ASC
+                    OFFSET @Offset ROWS
+                    FETCH NEXT @PageSize ROWS ONLY
+                END
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP PROCEDURE IF EXISTS getEmployeePagination");
+        }
+    }
+}
