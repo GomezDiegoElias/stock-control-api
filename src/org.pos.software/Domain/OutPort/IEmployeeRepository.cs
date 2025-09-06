6c5516277d3a15275b8142ded0c648c1f4c141e4
Diego <anikilogomez123@gmail.com>
2025-09-06 16:36:37 -0300

    feat(employee): agrega metodo para buscar empleado por dni en el repositorio

--- src/org.pos.software/Domain/OutPort/IEmployeeRepository.cs
+++ src/org.pos.software/Domain/OutPort/IEmployeeRepository.cs
@@ -6,5 +6,6 @@ namespace org.pos.software.Domain.OutPort
     public interface IEmployeeRepository
     {
         public Task<PaginatedResponse<Employee>> FindAll(int pageIndex, int pageSize, int? dni, string? firstname, string? lastname, string? workstation);
+        public Task<Employee?> FindByDni(long dni);
     }
 }
