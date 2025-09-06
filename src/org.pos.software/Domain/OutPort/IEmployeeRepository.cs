a0f7017f1bdbe669a49432b1f0b4f81d966aab80
Diego <anikilogomez123@gmail.com>
2025-09-05 21:46:25 -0300

    feat(employee): agrega metodo para paginacion de empleado en el repositorio

---
+++ src/org.pos.software/Domain/OutPort/IEmployeeRepository.cs
@@ -0,0 +1,10 @@
+﻿using org.pos.software.Domain.Entities;
+using org.pos.software.Infrastructure.Rest.Dto.Response.General;
+
+namespace org.pos.software.Domain.OutPort
+{
+    public interface IEmployeeRepository
+    {
+        public Task<PaginatedResponse<Employee>> FindAll(int pageIndex, int pageSize);
+    }
+}
