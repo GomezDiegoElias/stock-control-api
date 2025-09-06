f1e2efec2d0b8bc9a50c1fa587d5c638fff1a65b
Diego <anikilogomez123@gmail.com>
2025-09-06 16:32:25 -0300

    feat(employee): agrega excepcion personalizada para empleado

---
+++ src/org.pos.software/Domain/Exceptions/EmployeeNotFoundException.cs
@@ -0,0 +1,8 @@
+﻿namespace org.pos.software.Domain.Exceptions
+{
+    public class EmployeeNotFoundException : Exception
+    {
+        public EmployeeNotFoundException(long dni)
+            : base($"Employee with DNI {dni} not found.") { }
+    }
+}
