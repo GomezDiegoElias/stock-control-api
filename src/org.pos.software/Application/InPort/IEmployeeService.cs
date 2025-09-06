ab31b733870632f3b9de78f24c8555f52c23dce2
Diego <anikilogomez123@gmail.com>
2025-09-05 21:47:27 -0300

    feat(employee): agrega metodo de paginacion de empleado en el servicio

---
+++ src/org.pos.software/Application/InPort/IEmployeeService.cs
@@ -0,0 +1,10 @@
+﻿using org.pos.software.Domain.Entities;
+using org.pos.software.Infrastructure.Rest.Dto.Response.General;
+
+namespace org.pos.software.Application.InPort
+{
+    public interface IEmployeeService
+    {
+        public Task<PaginatedResponse<Employee>> FindAll(int pageIndex, int pageSize);
+    }
+}
